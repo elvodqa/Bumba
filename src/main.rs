ff901abc63c4f03df1eeaefd11eb8b6adde28d71
Emir Cengiz <emircengiz21@gmail.com>
2023-01-12 21:48:08 +0300

    Button

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod ui;
 
+use sfml::{graphics::*, window::{ContextSettings, Style, Event}, system::Vector2f};
 use crate::ui::*;
 
-use sfml::{graphics::*, window::{ContextSettings, Style, Event}, system::Vector2f};
 
 fn main() {
     let mut window = RenderWindow::new(
         (800, 600),
         "Bumba",
-        Style::CLOSE,
+        Style::DEFAULT,
         &ContextSettings::default(),
     );
     window.set_vertical_sync_enabled(true);
@@ -18,22 +18,34 @@ fn main() {
         Vector2f::new(800.0, 600.0)
     );
 
+    let mut some_button = Button::new(0, 0, 100, 50, "Fuck fof".to_string(), Color::RED, Color::GREEN, Color::BLUE, Color::WHITE);
+
     while window.is_open() {
         while let Some(ev) = window.poll_event() {
             match ev {
                 Event::Closed => window.close(),
                 Event::Resized { width, height } => {
-                    // resize the camera
                     camera.set_size(Vector2f::new(width as f32, height as f32));
                     window.set_view(&camera);
+                },
+                Event::MouseButtonPressed { button, x, y } => {
+                    some_button.poll_event(button, x, y);
                 },
                 _ => {}
             }
         }
 
+        some_button.update(window.mouse_position());
+
+        if some_button.is_hovered() {
+            //println!("Hovered");
+        }
+        if some_button.is_down() {
+            println!("Pressed");
+        }
+
         window.clear(Color::BLACK);
-        
-        
+        some_button.draw(&mut window);
         window.display();
     }
 
--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,4 @@
-use sfml::{graphics::{RenderWindow, Color}, system::Vector2i};
+use sfml::{graphics::{RenderWindow, Color, RenderTarget, Transformable, Shape}, system::{Vector2i, Vector2f}};
 
 
 
@@ -14,18 +14,80 @@ pub struct Button {
     pub text_color: Color,
     pub pressed: bool,
     pub hover: bool,
+    pub down: bool,
 }
 
 pub trait TButton {
     fn new(x: i32, y: i32, width: i32, height: i32, text: String, color: Color, hover_color: Color, pressed_color: Color, text_color: Color) -> Button;
     fn draw(&self, window: &mut RenderWindow);
     fn update(&mut self, mouse_pos: Vector2i);
+    fn poll_event(&self, button: sfml::window::mouse::Button, x :i32, y: i32);
     fn is_pressed(&self) -> bool;
+    fn is_hovered(&self) -> bool;
+    fn is_down(&self) -> bool;
 }
 
 impl TButton for Button {
     fn new(x: i32, y: i32, width: i32, height: i32, text: String, color: Color, hover_color: Color, pressed_color: Color, text_color: Color) -> Button {
-        
+        Button {
+            x,
+            y,
+            width,
+            height,
+            text,
+            color,
+            hover_color,
+            pressed_color,
+            text_color,
+            pressed: false,
+            hover: false,
+            down: false,
+        }
     }
+
+    fn draw(&self, window: &mut RenderWindow) {
+        let mut rect = sfml::graphics::RectangleShape::new();
+        rect.set_size((self.width as f32, self.height as f32));
+        rect.set_position((self.x as f32, self.y as f32));
+        rect.set_fill_color(if self.pressed { self.pressed_color } else if self.hover { self.hover_color } else { self.color });
+        let binding = sfml::graphics::Font::from_file("assets/fonts/Roboto-Regular.ttf").unwrap();
+        let mut text = sfml::graphics::Text::new(&self.text, &binding, 20);
+        text.set_fill_color(self.text_color);
+        text.set_position((self.x as f32 + self.width as f32 / 2.0 - text.local_bounds().width / 2.0, self.y as f32 + self.height as f32 / 2.0 - text.local_bounds().height / 2.0));
+
+        window.draw(&rect);
+        window.draw(&text);
+    }
+
+    fn update(&mut self, mouse_pos: Vector2i) {
+        if mouse_pos.x > self.x && mouse_pos.x < self.x + self.width && mouse_pos.y > self.y && mouse_pos.y < self.y + self.height {
+            self.hover = true;
+            if sfml::window::mouse::Button::Left.is_pressed() {
+                self.down = true;
+            } else {
+                self.down = false;
+            }
+        } else {
+            self.down = false;
+            self.hover = false
+        }
+    }
+
+    fn poll_event(&self, button: sfml::window::mouse::Button, x :i32, y: i32) {
+       println!("{} {}", x, y);
+    }
+
+    fn is_down(&self) -> bool {
+        self.down
+    }
+
+    fn is_pressed(&self) -> bool {
+        self.pressed
+    }
+
+    fn is_hovered(&self) -> bool {
+        self.hover
+    }
+    
 }
 
